f2d1cb968bb7a060213c0fe438a5b3e6558673bd
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2026-09-19 16:28:57 -0300

    Criado conexão ao SQLite - Retirado WeatherForecast

---
+++ AppExtensionista/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AppExtensionista.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppExtensionista.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<AlimentoModel> Alimentos { get; set; }
+        public DbSet<GuiaArmazenamentoModel> GuiasArmazenamento { get; set; }
+        public DbSet<EstoqueModel> Estoques { get; set; }
+        public DbSet<RegistroDescarteModel> RegistrosDescarte { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Desativar deleção em cascata globalmente ou por relacionamento
+            modelBuilder.Entity<RegistroDescarteModel>()
+                .HasOne(rd => rd.Estoque)
+                .WithMany()
+                .HasForeignKey(rd => rd.IdEstoque)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- AppExtensionista/Program.cs
+++ AppExtensionista/Program.cs
@@ -1,17 +1,42 @@
+using AppExtensionista.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuração do DbContext com SQLite
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
 
-builder.Services.AddControllers();
+// Controllers e Views
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Garante que o banco .db e as tabelas sejam criadas automaticamente na inicialização
+// Não recria o banco caso ele já exista
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
+
+// Configurações do Pipeline HTTP
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
